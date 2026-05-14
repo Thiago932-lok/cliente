7607b4d4387e369bf1ff78bbdc80c73f786de311
Thiago Mesquita <tmesqsilva@gmail.com>
2026-05-14 12:12:38 -0300

    projeto finalizado

--- cliente/urls.py
+++ cliente/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('exculir-cliente/<int:id>/', views.excluir_cliente, name='excluir_cliente'),
     path('login/', views.login_usuario, name='login_usuario'),
     path('novo-usuario/', views.novo_usuario, name='novo_usuario'),
+    path('Sair/', views.sair, name='sair'),
     path('', views.novo_cliente, name='novo_cliente'),
 ]
 
